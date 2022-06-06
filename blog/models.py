c942109befb7f09ea3144bb8c0de6403bb974f93
Iljas <naruto7777-998@mail.ru>
2022-06-06 19:37:25 +0500

    Add App Blog

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Post(models.Model):
- 	posts_image = models.ImageField(upload_to ="")
+ 	posts_image = models.ImageField(upload_to ="post_images")
  	posts_text = models.CharField(max_length = 300)
  	posts_data_time = models.DateTimeField()
  	posts_title = models.CharField(max_length = 50)
