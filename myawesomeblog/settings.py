d0526d7cce578be22030c8a95da6c8939e6b3a9c
Iljas <naruto7777-998@mail.ru>
2022-06-06 19:56:04 +0500

    Add App Blog

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT =  BASE_DIR / 'media' # этот код мы импортировали в urls при помощи settings
+MEDIA_ROOT =  BASE_DIR / "media" # этот код мы импортировали в urls при помощи settings
 MEDI_URL = '/awesome_media/'              # эта строка отвечает за то что увидит пользователь на ссылке 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
